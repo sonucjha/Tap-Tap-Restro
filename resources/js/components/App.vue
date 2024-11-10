a42e36ef31d90d0de7dc8a0d97dbdf67681a9c2f
sonu <sonucjha@gmail.com>
2024-11-10 15:17:30 +0530

    install vue 3 in laravel project

---
+++ resources/js/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello, Laravel with Vite and Vue!',
+        };
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b883;
+}
+</style>
+
