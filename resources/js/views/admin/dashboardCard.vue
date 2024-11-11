3e9a18610ae846c119e546115cbec9884960be6d
sonu <sonucjha@gmail.com>
2024-11-11 19:31:39 +0530

    add image folder in public folder

---
+++ resources/js/views/admin/dashboardCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="cards">
+        <div class="card">
+            <img src="./../../../../public/backEnd/images/small/img-3.jpg" alt="Today Order">
+            <h3>Today Order</h3>
+            <button>For More Detail</button>
+        </div>
+        <div class="card">
+            <img src="./../../../../public/backEnd/images/small/img-3.jpg" alt="Weekly Order">
+            <h3>Weekly Order</h3>
+            <button>For More Detail</button>
+        </div>
+        <div class="card">
+            <img src="./../../../../public/backEnd/images/small/img-3.jpg" alt="Monthly Order">
+            <h3>Monthly Order</h3>
+            <button>For More Detail</button>
+        </div>
+        <div class="card">
+            <img src="./../../../../public/backEnd/images/small/img-3.jpg" alt="Left Kirana Item">
+            <h3>Left Kirana Item</h3>
+            <button>For More Detail</button>
+        </div>
+    </div>
+</template>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TAP TAP</title>
     <!-- Link to the custom CSS file -->
-    <link rel="stylesheet" href="{{ asset('backEnd/adminLayout.css') }}">
-    <link rel="stylesheet" href="{{ asset('backEnd/common.css') }}">
-    <link rel="stylesheet" href="{{ asset('backEnd/footer.css') }}">
-    <link rel="stylesheet" href="{{ asset('backEnd/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('backEnd/sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('backEnd/css/adminLayout.css') }}">
+    <link rel="stylesheet" href="{{ asset('backEnd/css/common.css') }}">
+    <link rel="stylesheet" href="{{ asset('backEnd/css/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('backEnd/css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('backEnd/css/sidebar.css') }}">
+    <link rel="stylesheet" href="{{ asset('backEnd/css/dashboardCard.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
