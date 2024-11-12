6ebef4cdc12cbed2cc5ddabbef4526cbc6c068c5
sonu <sonucjha@gmail.com>
2024-11-12 15:20:31 +0530

    improve code

--- resources/js/views/admin/sidebar.vue
+++ resources/js/views/admin/sidebar.vue
@@ -1,86 +1,79 @@
 <template>
-    <div class="sidebar">
-      <ul class="menu">
-        <li><i class="icon">🏠</i> Dashboard</li>
-        <li>
-          <div @click="toggleDropdown('staff')" class="dropdown-toggle">
-            <i class="icon">👨‍💼</i> Staff
-          </div>
-          <ul v-show="dropdowns.staff" class="dropdown-menu">
-            <li>Staff List</li>
-            <li>Roles</li>
-          </ul>
-        </li>
-        <li><i class="icon">👥</i> Customer</li>
-        <li>
-          <div @click="toggleDropdown('menuManagement')" class="dropdown-toggle">
-            <i class="icon">📋</i> Menu Management
-          </div>
-          <ul v-show="dropdowns.menuManagement" class="dropdown-menu">
-            <li>View Menus</li>
-            <li>Add Menu</li>
-          </ul>
-        </li>
-        <li>
-          <div @click="toggleDropdown('kiranaItem')" class="dropdown-toggle">
-            <i class="icon">🍎</i> Kirana Item
-          </div>
-          <ul v-show="dropdowns.kiranaItem" class="dropdown-menu">
-            <li>Items List</li>
-            <li>Add Item</li>
-          </ul>
-        </li>
-        <li>
-          <div @click="toggleDropdown('order')" class="dropdown-toggle">
-            <i class="icon">📦</i> Order
-          </div>
-          <ul v-show="dropdowns.order" class="dropdown-menu">
-            <li>Orders</li>
-            <li>Order History</li>
-          </ul>
-        </li>
-        <li><i class="icon">⭐</i> Customer Review</li>
-        <li>
-          <div @click="toggleDropdown('photo')" class="dropdown-toggle">
-            <i class="icon">📸</i> Photo
-          </div>
-          <ul v-show="dropdowns.photo" class="dropdown-menu">
-            <li>Gallery</li>
-            <li>Upload Photo</li>
-          </ul>
-        </li>
-        <li>
-          <div @click="toggleDropdown('video')" class="dropdown-toggle">
-            <i class="icon">🎥</i> Video
-          </div>
-          <ul v-show="dropdowns.video" class="dropdown-menu">
-            <li>Video List</li>
-            <li>Upload Video</li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        dropdowns: {
-          staff: false,
-          menuManagement: false,
-          kiranaItem: false,
-          order: false,
-          photo: false,
-          video: false,
-        },
-      };
-    },
-    methods: {
-      toggleDropdown(menu) {
-        this.dropdowns[menu] = !this.dropdowns[menu];
-      },
-    },
-  };
-  </script>
-  
+  <div class="sidebar">
+    <ul class="menu">
+      <li><i class="icon">🏠</i> Dashboard</li>
+      <li>
+        <div @click="toggleDropdown('staff')" class="dropdown-toggle">
+          <i class="icon">👨‍💼</i> Staff
+        </div>
+        <ul v-show="dropdowns.staff" class="dropdown-menu">
+          <li>Staff List</li>
+          <li>Roles</li>
+        </ul>
+      </li>
+      <li><i class="icon">👥</i> Customer</li>
+      <li>
+        <div @click="toggleDropdown('menuManagement')" class="dropdown-toggle">
+          <i class="icon">📋</i> Menu Management
+        </div>
+        <ul v-show="dropdowns.menuManagement" class="dropdown-menu">
+          <li>View Menus</li>
+          <li>Add Menu</li>
+        </ul>
+      </li>
+      <li>
+        <div @click="toggleDropdown('kiranaItem')" class="dropdown-toggle">
+          <i class="icon">🍎</i> Kirana Item
+        </div>
+        <ul v-show="dropdowns.kiranaItem" class="dropdown-menu">
+          <li>Items List</li>
+          <li>Add Item</li>
+        </ul>
+      </li>
+      <li>
+        <div @click="toggleDropdown('order')" class="dropdown-toggle">
+          <i class="icon">📦</i> Order
+        </div>
+        <ul v-show="dropdowns.order" class="dropdown-menu">
+          <li>Orders</li>
+          <li>Order History</li>
+        </ul>
+      </li>
+      <li><i class="icon">⭐</i> Customer Review</li>
+      <li>
+        <div @click="toggleDropdown('photo')" class="dropdown-toggle">
+          <i class="icon">📸</i> Photo
+        </div>
+        <ul v-show="dropdowns.photo" class="dropdown-menu">
+          <li>Gallery</li>
+          <li>Upload Photo</li>
+        </ul>
+      </li>
+      <li>
+        <div @click="toggleDropdown('video')" class="dropdown-toggle">
+          <i class="icon">🎥</i> Video
+        </div>
+        <ul v-show="dropdowns.video" class="dropdown-menu">
+          <li>Video List</li>
+          <li>Upload Video</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref,reactive } from "vue";
+const dropdowns = reactive({
+  staff: false,
+  menuManagement: false,
+  kiranaItem: false,
+  order: false,
+  photo: false,
+  video: false,
+});
+
+const toggleDropdown = (menu) => {
+  dropdowns[menu] = !dropdowns[menu];
+};
+</script>
