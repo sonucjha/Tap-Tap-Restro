e5ef45786497363313533bfea0a95d706b77f87c
sonu <sonucjha@gmail.com>
2024-11-12 08:36:19 +0530

    improve code

--- resources/js/views/admin/sidebar.vue
+++ resources/js/views/admin/sidebar.vue
@@ -1,7 +1,5 @@
 <template>
-    <h1 class="visually-hidden">Sidebars examples</h1>
-
-    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark custom-sidebar" style="width: 280px;">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
             <svg class="bi me-2" width="40" height="32">
                 <use xlink:href="#bootstrap" />
