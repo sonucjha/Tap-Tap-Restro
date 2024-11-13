0100608fce490f02ad03991e691ca2eba61d0a20
sonu <sonucjha@gmail.com>
2024-11-14 00:23:32 +0530

    improve code

--- resources/js/views/admin/sidebar.vue
+++ resources/js/views/admin/sidebar.vue
@@ -3,13 +3,8 @@
     <div class=" brand-backgound">
         <h4 class="mb-1" style="color:white">Tap-Tap-Restro</h4>
       </div>
+
     <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <i class="bi bi-house-door me-2"></i>
-          Home
-        </a>
-      </li>
       <li>
         <a href="#" class="nav-link text-white">
           <i class="bi bi-speedometer2 me-2"></i>
@@ -17,51 +12,87 @@
         </a>
       </li>
       <li>
-        <a href="#" class="nav-link text-white">
-          <i class="bi bi-table me-2"></i>
-          Orders
+        <a href="#" class="nav-link text-white" @click.prevent="toggleDropdown('staff')">
+          <i class="bi bi-people-fill me-2"></i>
+          Staff
+          <i :class="dropdowns.staff ? 'bi bi-chevron-up' : 'bi bi-chevron-down'" class="float-end"></i>
         </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <i class="bi bi-grid me-2"></i>
-          Products
-        </a>
+        <ul v-if="dropdowns.staff" class="nav flex-column ms-3">
+          <li><a href="#" class="nav-link text-white">View Staff</a></li>
+          <li><a href="#" class="nav-link text-white">Add Staff</a></li>
+        </ul>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
           <i class="bi bi-people me-2"></i>
-          Customers
+          Customer 
         </a>
       </li>
-      <!-- New Dropdown -->
       <li>
-        <div class="dropdown">
-          <a 
-            class="nav-link dropdown-toggle text-white" 
-            href="#" 
-            id="dropdownMenuButton1" 
-            data-bs-toggle="dropdown" 
-            aria-expanded="false">
-            <i class="bi bi-list me-2"></i>
-            More Options
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton1">
-            <li><a class="dropdown-item" href="#">Option 1</a></li>
-            <li><a class="dropdown-item" href="#">Option 2</a></li>
-            <li><a class="dropdown-item" href="#">Option 3</a></li>
-          </ul>
-        </div>
+        <a href="#" class="nav-link text-white" @click.prevent="toggleDropdown('menuManagement')">
+          <i class="bi bi-list-ul me-2"></i>
+          Menu Management
+          <i :class="dropdowns.menuManagement ? 'bi bi-chevron-up' : 'bi bi-chevron-down'" class="float-end"></i>
+        </a>
+        <ul v-if="dropdowns.menuManagement" class="nav flex-column ms-3">
+          <li><a href="#" class="nav-link text-white">View Menu</a></li>
+          <li><a href="#" class="nav-link text-white">Add Menu</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white" @click.prevent="toggleDropdown('kiranaItem')">
+          <i class="bi bi-box-seam me-2"></i>
+          Kirana Item
+          <i :class="dropdowns.kiranaItem ? 'bi bi-chevron-up' : 'bi bi-chevron-down'" class="float-end"></i>
+        </a>
+        <ul v-if="dropdowns.kiranaItem" class="nav flex-column ms-3">
+          <li><a href="#" class="nav-link text-white">View Items</a></li>
+          <li><a href="#" class="nav-link text-white">Add Item</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white" @click.prevent="toggleDropdown('order')">
+          <i class="bi bi-cart-fill me-2"></i>
+          Orders
+          <i :class="dropdowns.order ? 'bi bi-chevron-up' : 'bi bi-chevron-down'" class="float-end"></i>
+        </a>
+        <ul v-if="dropdowns.order" class="nav flex-column ms-3">
+          <li><a href="#" class="nav-link text-white">View Orders</a></li>
+          <li><a href="#" class="nav-link text-white">Manage Orders</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <i class="bi bi-person-circle me-2"></i>
+          Customer Review
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white" @click.prevent="toggleDropdown('photo')">
+          <i class="bi bi-image me-2"></i>
+          Photo
+          <i :class="dropdowns.photo ? 'bi bi-chevron-up' : 'bi bi-chevron-down'" class="float-end"></i>
+        </a>
+        <ul v-if="dropdowns.photo" class="nav flex-column ms-3">
+          <li><a href="#" class="nav-link text-white">View Photos</a></li>
+          <li><a href="#" class="nav-link text-white">Add Photo</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white" @click.prevent="toggleDropdown('video')">
+          <i class="bi bi-camera-video-fill me-2"></i>
+          Video
+          <i :class="dropdowns.video ? 'bi bi-chevron-up' : 'bi bi-chevron-down'" class="float-end"></i>
+        </a>
+        <ul v-if="dropdowns.video" class="nav flex-column ms-3">
+          <li><a href="#" class="nav-link text-white">View Videos</a></li>
+          <li><a href="#" class="nav-link text-white">Add Video</a></li>
+        </ul>
       </li>
     </ul>
     <hr>
     <div class="dropdown">
-      <a 
-        href="#" 
-        class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" 
-        id="dropdownUser1" 
-        data-bs-toggle="dropdown" 
-        aria-expanded="false">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
         <strong>mdo</strong>
       </a>
